18e9af238405639306142505c802b4bfaae0d182
shivakrishna kavali <shivakavali0802@gmail.com>
2026-05-27 13:43:28 +0530

    Fix Render environment variables

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -1,70 +1,68 @@
-from supabase import create_client
-from dotenv import load_dotenv
-
 import os
+
+from dotenv import load_dotenv
+from supabase import create_client
 
 load_dotenv()
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-SUPABASE_URL = os.getenv(
-    "SUPABASE_URL"
-)
+if not SUPABASE_URL:
+    raise Exception("SUPABASE_URL missing")
 
-SUPABASE_KEY = os.getenv(
-    "SUPABASE_KEY"
-)
-
+if not SUPABASE_KEY:
+    raise Exception("SUPABASE_KEY missing")
 
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
 
-
-# =========================
+# ====================================
 # SAVE CHAT
-# =========================
+# ====================================
 
-def save_chat(
-    user_id,
-    title,
-    messages
-):
+def save_chat(user_id, messages):
 
-    data = {
+    title = "New Conversation"
+
+    for msg in messages:
+
+        if msg["sender"] == "user":
+
+            title = msg["text"][:30]
+            break
+
+    response = supabase.table(
+        "saved_chats"
+    ).insert({
 
         "user_id": user_id,
 
         "title": title,
 
         "messages": messages
-    }
 
-    response = (
-        supabase
-        .table("saved_chats")
-        .insert(data)
-        .execute()
-    )
+    }).execute()
 
     return response.data
 
 
-# =========================
+# ====================================
 # GET SAVED CHATS
-# =========================
+# ====================================
 
-def get_saved_chats(
-    user_id
-):
+def get_saved_chats(user_id):
 
-    response = (
-        supabase
-        .table("saved_chats")
-        .select("*")
-        .eq("user_id", user_id)
-        .order("id", desc=True)
-        .execute()
-    )
+    response = supabase.table(
+        "saved_chats"
+    ).select("*").eq(
+        "user_id",
+        user_id
+    ).order(
+        "created_at",
+        desc=True
+    ).execute()
 
     return response.data
